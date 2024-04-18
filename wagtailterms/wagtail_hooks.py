5300ebb560e23fa5c8217345e691d1422bded509
smark-1 <75799735+smark-1@users.noreply.github.com>
2024-04-17 20:48:26 -0400

    remove nested "'s in f strings so works in python v 3.11 and down

--- wagtailterms/wagtail_hooks.py
+++ wagtailterms/wagtail_hooks.py
@@ -19,7 +19,8 @@ TERM_ICON = get_setting('icon')
 def editor_js():
     # add the path to the terms list view to the javascript so that the url can be set dynamically for the terms api
     path_to_term = reverse("wagtailterms:terms-list")
-    return mark_safe(f'<script>const WAGTAIL_TERM_PATH = "{path_to_term}"; const WAGTAIL_TERM_STYLE = "{get_setting('style')}"</script>')
+    wagtail_term_styles = get_setting('style')
+    return mark_safe(f'<script>const WAGTAIL_TERM_PATH = "{path_to_term}"; const WAGTAIL_TERM_STYLE = "{wagtail_term_styles}"</script>')
 
 
 @hooks.register("register_rich_text_features")
