44db90e153aa38ad390b877442ab633eddf8ccf6
smark-1 <75799735+smark-1@users.noreply.github.com>
2024-04-30 23:38:21 -0400

    Add quick start
    * Added a quick start include template

--- example/home/models.py
+++ example/home/models.py
@@ -9,6 +9,14 @@ class HomePage(Page):
     pass
 
 
+class QuickStart(Page):
+    content = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("content")
+    ]
+
+
 class BasicPage(Page):
     content = RichTextField()
 
@@ -19,12 +27,12 @@ class BasicPage(Page):
 
 class AdvancedPage(Page):
     content = StreamField(
-        [
-            ("heading", blocks.CharBlock(form_classname="title")),
-            ("paragraph", blocks.RichTextBlock()),
-        ],
-        blank=True,
-        use_json_field=True,
+            [
+                ("heading", blocks.CharBlock(form_classname="title")),
+                ("paragraph", blocks.RichTextBlock()),
+            ],
+            blank=True,
+            use_json_field=True,
     )
 
     content_panels = Page.content_panels + [
